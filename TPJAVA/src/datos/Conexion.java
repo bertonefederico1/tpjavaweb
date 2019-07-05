642ccfbe6fe3eaa5f51a59ade1745d3cd92a5efa
Federico <Federico@N35>
2019-07-05 08:30:25 -0300

    servlet

--- TPJAVA/src/datos/Conexion.java
+++ TPJAVA/src/datos/Conexion.java
@@ -4,6 +4,8 @@ import java.sql.*;
 
 public class Conexion {
 
+	private static Conexion instancia;
+	
 	private String driver = "com.mysql.jdbc.Driver";
 	private String host = "localhost";
 	private String port = "3306";
@@ -20,19 +22,19 @@ public class Conexion {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static Conexion getInstancia() {
 		if (instancia == null) {
 			instancia = new Conexion();
 		}
 		return instancia;
 	}
-
+	
 	public Connection getConn() {
 		try {
-			if (conn == null || conn.isClosed()) {
-				conn = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db,user,password);
-				conectados = 0;
+			if(conn==null || conn.isClosed()) {
+				conn=DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db, user, password);
+				conectados=0;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -40,16 +42,17 @@ public class Conexion {
 		conectados++;
 		return conn;
 	}
-
+	
 	public void releaseConn() {
 		conectados--;
 		try {
-			if (conectados <= 0) {
+			if (conectados<=0) {
 				conn.close();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+		
 
 }
