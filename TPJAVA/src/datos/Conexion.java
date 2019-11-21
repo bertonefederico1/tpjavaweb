24abcf8503f65885a74bc12b6e3b16abb873eaac
Federico <Federico@N35>
2019-11-21 09:25:13 -0300

    prueba merge

--- TPJAVA/src/datos/Conexion.java
+++ TPJAVA/src/datos/Conexion.java
@@ -7,7 +7,7 @@ public class Conexion {
 	private static Conexion instancia;
 	
 	private String driver = "com.mysql.jdbc.Driver";
-	private String host = "localhost";
+	private String host = "localhost"; //modificado fede
 	//private String host = "N35";
 	private String port = "3306";
 	private String user = "root";
