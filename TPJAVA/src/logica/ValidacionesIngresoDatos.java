9597bb7d115ab6448bea4a0fa213216850492224
andresbertone <andresbertone97@gmail.com>
2019-11-25 09:28:01 -0300

    ARREGLOS EN FACTURAR

--- TPJAVA/src/logica/ValidacionesIngresoDatos.java
+++ TPJAVA/src/logica/ValidacionesIngresoDatos.java
@@ -3,7 +3,7 @@ package logica;
 public class ValidacionesIngresoDatos {
 	
 	public boolean ingresoYClienteVacio(String dni, String cod_reparacion_string){
-		if((dni.equalsIgnoreCase("Cliente") || (cod_reparacion_string.equalsIgnoreCase("Reparacion")))){
+		if(dni.equalsIgnoreCase("Cliente") || (cod_reparacion_string.equalsIgnoreCase("Reparacion"))){
 			return true;
 		}else{
 			return false;
